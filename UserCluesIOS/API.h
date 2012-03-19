b973523d6165818e5e6e050667f31b2e9324682b
Kevin Sornberger <ksornberger@gmail.com>
2012-03-19 18:27:02 -0400

    Adding API Class

---
+++ UserCluesIOS/API.h
@@ -0,0 +1,13 @@
+//
+//  API.h
+//  UserCluesIOS
+//
+//  Created by Kevin Sornberger on 12-03-19.
+//  Copyright (c) 2012 DevBBQ Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface API : NSObject
+
+@end
---
+++ UserCluesIOS/API.m
@@ -0,0 +1,22 @@
+//
+//  API.m
+//  UserCluesIOS
+//
+//  Created by Kevin Sornberger on 12-03-19.
+//  Copyright (c) 2012 DevBBQ Inc. All rights reserved.
+//
+
+#import "API.h"
+
+@implementation API
+
+static API instance = nil;
+
++(API)instance{
+    if (!instance){
+        instance = [[[API alloc] init] autorelease];
+    }
+    return instance;
+}
+
+@end
